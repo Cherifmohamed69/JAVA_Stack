9519de3757407221edd3573d94d2532058fc8279
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-07-31 18:39:13 +0200

    push code

---
+++ Week 1/Day 1/Practice/FirstJavaProgram/First_java_program.java
@@ -0,0 +1,7 @@
+public class First_java_program {
+	public static void main(String[] args) {
+        System.out.println("My name is Cherif Mohamed");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Tunisia");
+	}
+}
